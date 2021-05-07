30a95b280f991238516e115cc10206ab1b572adf
Diogo Carvalho <34646795+Diogo-P-Carvalho@users.noreply.github.com>
2021-05-07 12:57:41 +0100

    environment variable

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = secret.UNSPLASH_API_KEY;
+const apiKey = UNSPLASH_API_KEY;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Helper function to set attributes on DOM elements
